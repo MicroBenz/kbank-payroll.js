0a12a6da9aacd8531d76ad299ae5be99a5de69b9
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2019-03-20 17:42:06 +0700

    Header

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-function test(num) {
-  return num;
+const dayjs = require('dayjs');
+const { padStart, padEnd: lodashPadEnd } = require('lodash');
+
+dayjs().format('{YYYY} MM-DDTHH:mm:ss SSS [Z] A');
+
+const defaultConfig = {
+  date: Date.now(),
+};
+
+const padEnd = (str, length) => lodashPadEnd(str, length).substr(0, length);
+
+function getTotalAmount(transactions) {
+  const totalAmount = transactions
+    .reduce((prev, curr) => {
+      return prev + curr.amount;
+    }, 0);
+  return (totalAmount * 100).toFixed(0);
 }
 
-module.exports = test;
+function smartPayroll(transactions = [], config) {
+  if (!config.companyName) {
+    throw new Error('config is invalid! companyName is required');
+  }
+  if (!config.accountNumber) {
+    throw new Error('config is invalid! accountNumber is required');
+  }
+  const mergedConfig = {
+    ...defaultConfig,
+    ...config,
+  };
+  const totalAmount = getTotalAmount(transactions);
+  const header = `HPCT                000000              ${mergedConfig.accountNumber} ${padStart(totalAmount, 15, 0)} ${dayjs(mergedConfig.date, 'DD-MM-YYYY').format('YYMMDD')}                         TMT Marketplace Pte. Ltd.                         ${dayjs(mergedConfig.date, 'DD-MM-YYYY').format('YYMMDD')}${padStart(transactions.length, 18, 0)}N     \n`;
+  return header;
+}
+
+function directCredit(transactions = []) {
+  return transactions;
+}
+
+module.exports = {
+  smartPayroll,
+  directCredit,
+};
--- index.spec.js
+++ index.spec.js
@@ -1,11 +1,51 @@
+const dayjs = require('dayjs');
 const kbank = require('./index');
 
-describe('Normal case', () => {
-  it('should return nothing', () => {
-    expect(kbank()).toEqual(undefined);
+describe('import module', () => {
+  it('should have 2 functions: smartPayroll and directCredit', () => {
+    const keys = Object.keys(kbank);
+    expect(keys.indexOf('smartPayroll')).not.toEqual(-1);
+    expect(keys.indexOf('directCredit')).not.toEqual(-1);
+  });
+});
+
+const failConfig = {
+
+};
+
+const config = {
+  companyName: 'TMT Marketplace Pte. Ltd.',
+  accountNumber: '1234567890',
+  date: new Date('02, 22 2019'),
+};
+
+describe('Smart Payroll', () => {
+  it('should correct, with only header', () => {
+    const res = kbank.smartPayroll([], config);
+    const header = 'HPCT                000000              1234567890 000000000000000 190222                         TMT Marketplace Pte. Ltd.                         190222000000000000000000N     \n';
+    expect(res.split('\n')).toHaveLength(2);
+    expect(res).toEqual(header);
   });
 
-  it('should return 3', () => {
-    expect(kbank(3)).toEqual(3);
+  it('should correct', () => {
+    const res = kbank.smartPayroll([
+      { name: 'Tananan Tangthanachaikul', amount: 63700, accountNumber: '1234567890' },
+      { name: 'Nontapat Piyamongkol', amount: 12000, accountNumber: '0123456789' },
+    ], config);
+    expect(res.split('\n')).toHaveLength(2);
+    const [header, content] = res.split('\n');
+    expect(header).toEqual('HPCT                000000              1234567890 000000007570000 190222                         TMT Marketplace Pte. Ltd.                         190222000000000000000002N     ');
   });
+
+  it('should throw error without company name', () => {
+    try {
+      kbank.smartPayroll([], failConfig);
+    } catch (e) {
+      expect(e.message).toBe('config is invalid! companyName is required');
+    }
+  });
+});
+
+describe('Direct Credit', () => {
+
 });
